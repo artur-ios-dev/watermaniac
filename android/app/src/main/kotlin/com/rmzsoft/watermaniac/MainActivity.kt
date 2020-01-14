7ad2d0bd7b3f982bfaf1c76958b2cb02445b2796
ShazaHassanAhmed <57171201+ShazaHassanAhmed@users.noreply.github.com>
2020-01-14 11:02:45 +0200

    fix a issue

--- android/app/src/main/kotlin/com/rmzsoft/watermaniac/MainActivity.kt
+++ android/app/src/main/kotlin/com/rmzsoft/watermaniac/MainActivity.kt
@@ -9,4 +9,10 @@ class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("enable-software-rendering", true)
+        super.onCreate(savedInstanceState)
+
+    }
 }
